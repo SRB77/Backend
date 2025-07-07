f462c34ef53de6730a7ff1f0b4721163869bf5dc
SRB77 <soumyach144@gmail.com>
2025-07-07 22:42:05 +0530

    server connected to mongoDB , learn what is compass , atlas db etc . , then used .env file to hide credential from source code in github .

---
+++ L5noteDB/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+require("dotenv").config();
+const connectToDB = require('./src/DB/db')
+const port = process.env.PORT;
+//? Middleware 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+//? ROUTES
+connectToDB();
+app.get('/',(req,res)=>{
+    res.send(`Welcome to the Notes aplication with DB`);
+})
+app.post('/createNote',(req,res)=>{
+    const {title,content} = req.body;
+    res.json({
+        title:title,
+        content:content
+    })
+    console.log(title,content);
+})
+
+app.listen(port,()=>{
+    console.log(`Server is running on ${port}`);
+})
