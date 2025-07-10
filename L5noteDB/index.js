67281c16b3f443e15c631dc257c1bcb8d101dfff
SRB77 <soumyach144@gmail.com>
2025-07-10 19:46:08 +0530

    minor changes ignore this commit

--- L5noteDB/index.js
+++ L5noteDB/index.js
@@ -1,25 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 require("dotenv").config();
-const connectToDB = require('./src/DB/db')
+const connectToDB = require("./src/DB/db");
 const port = process.env.PORT;
-//? Middleware 
+//? Middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 //? ROUTES
 connectToDB();
-app.get('/',(req,res)=>{
-    res.send(`Welcome to the Notes aplication with DB`);
-})
-app.post('/createNote',(req,res)=>{
-    const {title,content} = req.body;
-    res.json({
-        title:title,
-        content:content
-    })
-    console.log(title,content);
-})
+app.get("/", (req, res) => {
+  res.send(`Welcome to the Notes aplication with DB`);
+});
+app.post("/createNote", (req, res) => {
+  const { title, content } = req.body;
+  res.json({
+    title: title,
+    content: content,
+  });
+  console.log(title, content);
+});
 
-app.listen(port,()=>{
-    console.log(`Server is running on ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
--- L5noteDB/src/DB/db.js
+++ L5noteDB/src/DB/db.js
@@ -1,13 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 require("dotenv").config();
-//? Server kese DB se connect karna hai wo DB.js file mein rahega 
+//? Server kese DB se connect karna hai wo DB.js file mein rahega
 
-function connectToDB(){
-    mongoose.connect(
-      process.env.DB_URL
-    ).then(()=>{
-        console.log('Connected to DB')
-    })
+function connectToDB() {
+  mongoose.connect(process.env.DB_URL).then(() => {
+    console.log("Connected to DB");
+  });
 }
 
 module.exports = connectToDB;
