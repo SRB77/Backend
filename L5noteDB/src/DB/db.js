2c43dee6fd36902e5dd5b6906825b937c1d8506a
SRB77 <soumyach144@gmail.com>
2025-07-10 14:49:05 +0530

    some minor changes ignorable commit

--- L5noteDB/src/DB/db.js
+++ L5noteDB/src/DB/db.js
@@ -3,9 +3,11 @@ require("dotenv").config();
 //? Server kese DB se connect karna hai wo DB.js file mein rahega 
 
 function connectToDB(){
-    mongoose.connect(process.env.DB_URL).then(() => {
-      console.log("Connected to DB");
-    });
+    mongoose.connect(
+      process.env.DB_URL
+    ).then(()=>{
+        console.log('Connected to DB')
+    })
 }
 
 module.exports = connectToDB;
