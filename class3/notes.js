300bfae47a006274551bc815aa92fd796b3c6601
SRB77 <soumyach144@gmail.com>
2025-08-26 11:06:50 +0530

    Added another endpoint to get all the notes in Notes.txt

--- class3/notes.js
+++ class3/notes.js
@@ -1,30 +1,50 @@
-const express = require('express');
-const fs = require('fs/promises');
+const express = require("express");
+const fs = require("fs/promises");
 const PORT = 3009;
 
 const Notes = express();
 
 Notes.use(express.json());
 
-Notes.get('/',(req,res)=>{
-    res.status(200).send('The Code is running on root route');
-})
+Notes.get("/", (req, res) => {
+  res.status(200).send("The Code is running on root route");
+});
 
-Notes.post('/notes',(req,res)=>{
-    const noteData = req.body; 
-    async function addNote(){
-        try{
-            await fs.appendFile('Notes.txt', JSON.stringify(noteData) + '\n');
-            res.status(200).send(`Notes added succefully ${JSON.stringify(noteData)}`);
-        }
-        catch (err){
-            res.status(404).send(`${err}`);
-        }
-    } 
-    addNote();
-})
+Notes.post("/notes", (req, res) => {
+  const noteData = req.body;
+  async function addNote() {
+    try {
+      await fs.appendFile("Notes.txt", JSON.stringify(noteData) + "\n");
+      res
+        .status(200)
+        .send(`Notes added succefully ${JSON.stringify(noteData)}`);
+    } catch (err) {
+      res.status(404).send(`${err}`);
+    }
+  }
+  addNote();
+});
 
-Notes.listen(PORT , ()=>{
-    console.log(`Notes Server is running on ${PORT}`);
-})
+Notes.get("/allNotes", (req, res) => {
+  async function getAllNotes() {
+    try {
+      const content = await fs.readFile("Notes.txt", "utf-8");
+      res
+        .status(200)
+        .json({ message: "Retrive the Whole Notes", content: `${content}` });
+    } catch (err) {
+      res
+        .status(400)
+        .json({
+          error: `${err}`,
+          message: " May be some Error for getting the Content",
+        });
+    }
+  }
 
+  getAllNotes();
+});
+
+Notes.listen(PORT, () => {
+  console.log(`Notes Server is running on ${PORT}`);
+});
