d86195826fb0559b440803b84efe6fca56c79a57
SRB77 <soumyach144@gmail.com>
2025-08-27 22:50:43 +0530

    Hide the credentitals

--- class5/src/DB/db.js
+++ class5/src/DB/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 async function connectToDB(){
-        await mongoose.connect("mongodb+srv://chandanCohort:dIHzCxsgxhFK80Pg@cohort.ujea7jb.mongodb.net/cohort");
+        await mongoose.connect("URI from the compass");
         console.log('Connected TO DB 🥳')
 }
 
