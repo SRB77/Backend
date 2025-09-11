11ccc8db8bc77df0f9f656dbcb89cf077f3114fa
SRB77 <soumyach144@gmail.com>
2025-09-11 23:00:55 +0530

    all classes from 12-14 done now on the revison lecture of 14

--- class12_14/src/app.js
+++ class12_14/src/app.js
@@ -4,7 +4,7 @@ const cookieParser = require('cookie-parser');
 const app = express();
 
 app.use(express.json())
-app.use(cookieParser());
+app.use(cookieParser()); //> This is used for storing the token into the cookies  
 
 app.use('/auth' , authRoutes)
 module.exports = app;
